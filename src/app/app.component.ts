1137b42a5e9ad6675787fbaadf3763f91f9f15b1
DaiDH <40824445+id1945@users.noreply.github.com>
2026-03-14 23:52:15 +0700

    v1.8.0

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -58,14 +58,11 @@ export class AppComponent implements AfterViewInit {
   }
 
   public handle(action: any, fn: string): void {
-    // Fix issue #27, #29
-    const playDeviceFacingBack = (devices: any[]) => {
-      // front camera or back camera check here!
-      const device = devices.find(f => (/back|rear|environment/gi.test(f.label))); // Default Back Facing Camera
-      action.playDevice(device ? device.deviceId : devices[0].deviceId);
-    }
-
     if (fn === 'start') {
+      // Fix issue #27, #29
+      const playDeviceFacingBack = (devices: any[]) => {
+        action.playDevice(devices?.[0]?.deviceId);
+      }
       action[fn](playDeviceFacingBack).subscribe((r: any) => console.log(fn, r), alert);
     } else {
       action[fn]().subscribe((r: any) => console.log(fn, r), alert);
